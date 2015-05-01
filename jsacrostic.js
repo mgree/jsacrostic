ee21de1153c901121457f7e484c7b5c0a6d96084
Michael Greenberg <michael.m.greenberg@gmail.com>
2015-05-01 11:00:40 -0400

    basic histogram output

--- jsacrostic.js
+++ jsacrostic.js
@@ -653,10 +653,64 @@ makeHistogram = function (s) {
     return h;
 };
 
+showHistogram = function (h) {
+    var margin = {top: 20, right: 20, bottom: 30, left: 40},
+        width  = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+    
+    var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear().range([height, 0]);
+
+    var xAxis = d3.svg.axis().scale(x).orient("bottom");
+
+    var yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+
+    var svg = d3.select("#histogram").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    var data = [];
+    for (var c in h) {
+        data.push({ letter: c, occurences: h[c] });
+    }
+    console.log(h);
+    console.log(data);
+
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { return d.occurences; })]);
+
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+    
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Occurences");
+    
+    svg.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d.letter); })
+        .attr("width", x.rangeBand())
+        .attr("y", function(d) { return y(d.occurences); })
+        .attr("height", function(d) { return height - y(d.occurences); });
+};
+
 updateReadout = function () {
-    var quote = clueCharacters($("#quote").text());
-    var author = clueCharacters($("#author").prop("value"));
-    var title = clueCharacters($("#title").prop("value"));
+    var quote = clueCharacters($("#quote").val());
+    var author = clueCharacters($("#author").val());
+    var title = clueCharacters($("#title").val());
     var acrostic = author + title;
 
     var numClues = acrostic.length;
@@ -680,6 +734,8 @@ updateReadout = function () {
             "acrostic (author and title) that don't appear in the quote: " +
             badLetters.join(", "));
     }
+
+    showHistogram(hQuote);
         
 };
 
